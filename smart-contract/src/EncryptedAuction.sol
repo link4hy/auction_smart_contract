c2c8012c26f1f00eb4e298e351c03b6011adadff
link4hy <57839065+link4hy@users.noreply.github.com>
2025-02-05 13:24:52 +0700

    Update EncryptedAuction.sol

--- smart-contract/src/EncryptedAuction.sol
+++ smart-contract/src/EncryptedAuction.sol
@@ -66,7 +66,7 @@ contract EncryptedAuction {
         emit Bid(msg.sender, msg.value);
     }
 
-    function start(uint _openingBid, uint _duration) external onlyOwner {
+    function start(uint _duration) external onlyOwner {
         // validations
         require(!started, "Auction has already started");
 
